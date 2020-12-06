18939819e352448607f237ff59298b3b182458a8
Bob <robertamccullough@gmail.com>
2020-12-06 13:17:28 -0700

    updated build files

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5cfd38ae133f9f86b3928b2e2d146d52.js"
+  "/precache-manifest.1535cf349532798b2e97b119bbb7cf0c.js"
 );
 
 self.addEventListener('message', (event) => {
