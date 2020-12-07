534ec8a5c7bcca6421b10ff7ac8e80ee79268f04
Bob <robertamccullough@gmail.com>
2020-12-06 18:10:04 -0700

    added remainder of build files

---
+++ client/build/precache-manifest.db5e00b0e9d5b2a876a72b0539717e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53d8838771ca73c981b823583c86578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c5cb4312ba89cb9f0d",
+    "url": "/static/css/main.6b4d198d.chunk.css"
+  },
+  {
+    "revision": "bc99be2c4f569753799e",
+    "url": "/static/js/2.36bc9753.chunk.js"
+  },
+  {
+    "revision": "bc21d9e8b6a7110d3511ff1a6acbfd84",
+    "url": "/static/js/2.36bc9753.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49c5cb4312ba89cb9f0d",
+    "url": "/static/js/main.8d0f6e16.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "d77f2493d6b15e0099febcc6ad0ff87a",
+    "url": "/static/media/missing-avatar.d77f2493.png"
+  }
+]);
