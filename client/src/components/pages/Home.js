e5ce7740e219093e576a17fd6d50e5f2727b1785
Bob McCullough <bob.mccullough@optiosurgical.com>
2022-04-21 11:03:56 -0600

    remove see all button

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -36,12 +36,12 @@ class Home extends React.Component {
                 <h1 className={this.props.screenSize < 410 ? 'display-5' :'display-4'}>Welcome to OpenMats.org</h1>
                 <p className="lead mt-3" style={{fontWeight:'400'}}>Working hard to become the Internet's largest database of Brazilian Jiu Jitsu open mats.</p>
                 {/* <p className="lead mt-3">(Please help us by adding your gym!)</p> */}
-                <Link to="/allgyms" className="lead mt-3 font-weight-bold"><u>Currently {this.props.totals.mats} open mats at {this.props.totals.gyms} gyms in {this.props.totals.states} cities</u> <button className="btn btn-sm btn-primary ml-2">See All</button></Link>
-                {/* <Link to="/allgyms" className="lead mt-3 font-weight-bold"><u>Currently {this.props.totals.mats} open mats at {this.props.totals.gyms} gyms in {this.props.totals.states} cities</u> (<u>See All</u>)</Link> */}
-                <p className="lead text-danger mt-3 mb-5 font-weight-bold">Many gyms currently have restrictions around COVID-19, please call or email before arriving.</p>
-                {/* <p className="lead mt-3">All listings have been updated to reflect COVID-19 changes.</p> */}
-                {/* <hr className="my-4"></hr> */}
-                <form className="form my-2 my-lg-0 justify-content-center mx-auto" style={{maxWidth:'600px'}} value={this.state.searchTerm} onChange={e=>{this.setState({searchTerm:e.target.value})}} onSubmit={e=>{this.submitSearch(e)}}>
+                <p className='mb-4'>
+                    <Link to="/allgyms" className="lead mt-3 font-weight-bold"><u>Currently {this.props.totals.mats} open mats at {this.props.totals.gyms} gyms in {this.props.totals.states} cities</u> </Link>
+                </p>
+
+                {/* <p className="lead text-danger mt-3 mb-5 font-weight-bold">Many gyms currently have restrictions around COVID-19, please call or email before arriving.</p> */}
+                <form className="form mt- my-2 my-lg-0 justify-content-center mx-auto" style={{maxWidth:'600px'}} value={this.state.searchTerm} onChange={e=>{this.setState({searchTerm:e.target.value})}} onSubmit={e=>{this.submitSearch(e)}}>
                     <input className="form-control form-control-lg mr-sm-2" type="search" placeholder={this.props.screenSize > 500 ? "Enter a location to search for open mats..." : "Enter a search location..."} aria-label="Search" required></input>
                     <button className="btn btn-lg btn-primary my-3" type="submit">Search</button>
                 </form>
